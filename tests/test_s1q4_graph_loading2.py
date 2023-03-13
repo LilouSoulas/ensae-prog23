6ce1e6434d01e09df1d1c3fe04ce015274d10738
Lilou Soulas <lilou.soulas@gmail.com>
2023-03-13 20:25:25 +0100

    Rajout des trucs de Juliette , nouveau test

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,31 @@
 # À compléter
+
+import sys 
+sys.path.append("delivery_network/")
+
+import unittest 
+from graph import Graph, graph_from_file
+
+class Test_GraphLoading(unittest.TestCase):
+    def test_network0(self):
+        g = graph_from_file("input/network.00.in")
+        self.assertEqual(g.nb_nodes, 10)
+        self.assertEqual(g.nb_edges, 9)
+
+    def test_network1(self):
+        g = graph_from_file("input/network.01.in")  #aucune distance n'est donnée dans ce fichier, d'où le test
+        self.assertEqual(g.nb_nodes, 7)
+        self.assertEqual(g.nb_edges, 5)
+        self.assertEqual(g.graph[5][0][2])  #la fonction graph_from_file a été modifiée afin de faire apparaître la distance par défaut
+    
+    def test_network4(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.nb_nodes, 10)
+        self.assertEqual(g.nb_edges, 4)
+        self.assertEqual(g.graph[1][0][2], 6)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+## FAILED, est ce que Q4 a ete faite?
