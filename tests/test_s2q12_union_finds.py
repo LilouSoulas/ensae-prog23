276b2b5ef1d60752915dcedb16653cceabf9ee83
Lilou Soulas <lilou.soulas@gmail.com>
2023-03-15 22:59:46 +0100

    Ajout des dernières fonctions et tests

---
+++ tests/test_s2q12_union_finds.py
@@ -0,0 +1,31 @@
+import sys 
+sys.path.append("delivery_network/")
+
+# le test ne marche pas 
+
+import unittest 
+from graph import Graph, UnionFind
+
+class Test_UnionFinds(unittest.TestCase):
+    def test_0(self):
+        uf = UnionFind(6)
+        uf.union(1,2)
+        uf.union(2,3)
+        self.assertEqual(uf.find(1),1)
+        self.assertEqual(uf.find(2),3)
+
+    def test_1(self):
+        uf = UnionFind(10)
+        uf.union(5,8)
+        uf.union(2,7)
+        uf.union(1,10)
+        uf.union(5,3)
+        uf.union(9,10)
+        uf.union(5,9)
+        self.assertEqual(uf.find(1),5)
+        self.assertEqual(uf.find(5),5)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
